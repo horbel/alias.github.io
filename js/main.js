d50f3e902ecc8c6c32f2ecdceece8658cd5b4b63
Gorbel <gorbel.allex@gmail.com>
2017-01-03 22:45:32 +0300

    add swipe jquery plagin

---
+++ js/main.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $(".word-wrapper").swipe({
+        swipeUp: function (event, direction, distance, duration) {
+            $(".word-wrapper").css("background-color", "red");
+            console.log("You swiped " + direction)
+        },
+        swipeDown: function (event, direction, distance, duration) {
+            $(".word-wrapper").css("background-color", "green");
+            console.log("You swiped " + direction)
+        },
+        click: function (event, target) {
+        },
+        threshold: 100,
+        allowPageScroll: "vertical"
+    });
+});
