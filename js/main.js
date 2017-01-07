2cd94cb0d3d97103375b8cb37dc267f7a5fdb365
Gorbel <gorbel.allex@gmail.com>
2017-01-07 11:36:37 +0300

    thx sasha

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-﻿//$(document).ready(function () {
+﻿/// <reference path="../Scripts/angular.js" />
+/// <reference path="../Scripts/jquery.touchSwipe.js" />
+/// <reference path="../Scripts/jquery-1.9.1.js" />
+
+
+
+//$(document).ready(function () {
 //    $(".word-wrapper").swipe({
 //        swipeUp: function (event, direction, distance, duration) {
 //            $(".word-wrapper").css("background-color", "red");
@@ -17,31 +23,29 @@
 
 
 var aliasApp = angular.module("aliasApp", [])
-    .controller("startController", ["$scope", "$http", startCtrl]);
-function startCtrl($scope, $http) {
-    var teamForm = document.getElementById('teamForm');
-    var teamCounter = 2;
-    $scope.Teams = [];
-    $scope.Teams[0] = {  
-        name: "Команда 1",
+    .controller("startController", startCtrl);
+function startCtrl() {
+    var vm = this;
+
+    var teamObject = {
+        name: "Команда",
         score: 0
-    }
-    $scope.Teams[1] = {
-        name: "Команда 2",
-        score: 0
-    }
-    $scope.addTeam = function () {
-        teamCounter++;
-        var newTeamDomElement = document.createElement('input');
-        newTeamDomElement.setAttribute('class', 'form-control');
-        newTeamDomElement.setAttribute('type', 'text');
-        newTeamDomElement.setAttribute('ng-model', 'Teams[' + (teamCounter - 1) + '].name');    
-        newTeamDomElement.setAttribute('value', 'Команда ' + teamCounter);
-        var team = {
-            name: "Команда " + teamCounter,
+    };
+
+    vm.teams = [
+        {
+            name: "Команда",
             score: 0
-        };
-        $scope.Teams.push(team);
-        teamForm.appendChild(newTeamDomElement);
+        },
+        {
+            name: "Команда",
+            score: 0
+        }
+    ];
+
+    vm.addTeam = function () {
+        var team = angular.copy(teamObject);
+        team.name += " " + (vm.teams.length + 1);
+        vm.teams.push(team);
     }  
 }
